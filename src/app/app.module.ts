2d33872491ae3405e751d6fe63ba24e7439dd41f
ajithshine <ajithshine77@gmail.com>
2022-09-24 20:02:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // RouterModule,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
